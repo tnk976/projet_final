8bf5ba59da00d66a73865f7bfc982377aefb764a
tnk976 <badrane976@gmail.com>
2018-07-06 11:47:03 +0400

    ajout des fonction pour inscription et login pour particulier

--- controllers/particulierControllers.js
+++ controllers/particulierControllers.js
@@ -1,9 +1,41 @@
 var mongoose = require('mongoose');
 
 var particulierController = {};
-var Produit = require ("../models/Produit"); 
+var particulier = require ("../models/particulier"); 
 
-
+particulierController.list = function(req, res) {
+    User.find({}).exec(function(err, user){
+        if(err){
+            console.log('Error : ', err);
+        }else{
+            //console.log("->",produit);
+            res.render("../views/users/index",{user:user} );
+        } 
+    });
+  };
+  
+  // Créer un login
+  particulierController.create = function(req, res){
+    res.render("../views/users/profile");
+  }; 
+  
+  particulierController.save = function (req, res) {
+    if (req.body.username &&
+      req.body.password &&
+      req.body.passwordConfirmation) {
+  
+      var user = new User(req.body);
+      user.save(function (err) {
+        if (err) {
+          console.log(err);
+          res.render("../views/users/profile");
+        } else {
+          console.log("login OK");
+          res.redirect("/profiles");
+        }
+      });
+    };
+  }
 
 //export du module
 module.exports = particulierController;
--- routes/particuliers.js
+++ routes/particuliers.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var particulier = require("../controllers/particulierControllers");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('particulier/liste');
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('particulier/liste');
+// });
 
-router.get('/edit', function(req, res, next) {
-    res.render('particulier/ajoutcours');
-  });
+// router.get('/edit', function(req, res, next) {
+//     res.render('particulier/ajoutcours');
+//   });
 
+//recuperer la liste des users
+router.get("/", particulier.list);
 
+//creer un login
+router.get("/ajoutuser", particulier.create);
+
+//creer un login
+router.post("/save", particulier.save);
+
+//export du module router
 module.exports = router;
+
