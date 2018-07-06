f97109660f56c6c8ef11f8ae99a302e382141335
anna974 <annapiaraly@gmail.com>
2018-07-06 16:08:51 +0400

    ajout ateliers

--- routes/cuisiniers.js
+++ routes/cuisiniers.js
@@ -2,19 +2,23 @@ var express = require('express');
 var router = express.Router();
 var cuisinier = require("../controllers/cuisiniersController");
 
-//recuperer la liste des users
+//recuperer la liste des utilisateurs
 router.get("/", cuisinier.list);
 
-//creer un login
+//accéder à la page d'inscription
 router.get("/ajoutuser", cuisinier.create);
 
-//creer un login
+//Créer un utilisateur / cuisinier
 router.post("/save", cuisinier.save);
 
+// Accéder à la page de connexion
 router.get("/login", cuisinier.login);
 
 // Se connecter
 router.post('/auth', cuisinier.auth);
+
+// Se déconnecter
+router.get('/logout', cuisinier.logout);
 
 
 //export du module router
