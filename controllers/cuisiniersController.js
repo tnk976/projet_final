3f2786f38f7aafabcf4776f283ff132e03fa80c3
anna974 <annapiaraly@gmail.com>
2018-07-06 13:53:52 +0400

    ajout connexion cuisinier

--- controllers/cuisiniersController.js
+++ controllers/cuisiniersController.js
@@ -15,12 +15,12 @@ cuisinierController.list = function(req, res) {
   });
 };
 
-// Enregistrer un cuisinier
+// Renvoit à la page d'inscription
 cuisinierController.create = function (req, res) {
     res.render("../views/cuisinier/inscription");
   };
 
-  
+// Enregistrer un cuisinier  
 cuisinierController.save = function (req, res) {
   if (req.body.nom &&
     req.body.prenom &&
@@ -41,6 +41,36 @@ cuisinierController.save = function (req, res) {
   };
 }
 
+// Renvoit à la page de connexion
+cuisinierController.login = function (req, res) {
+  res.render("../views/cuisinier/connexion");
+};
+
+// fonction pour se connecter
+cuisinierController.auth = function (req, res) {
+  var email = req.body.Email;
+  var password = req.body.Password;
+
+  Cuisinier.findOne({ email: email }).exec(function (err, user) {
+    if (!err && user) {
+      bcrypt.compare(password, user.password, function (err, result) {
+        console.log(result);
+        if (result === true) {
+          req.session.userId = user._id;
+          req.session.Email = user.email;
+          req.session.success = 'Connexion Reussie';
+          res.redirect('/cuisiniers');
+        }else {
+        //console.log(req.session.userName);
+        res.redirect('/cuisiniers/login');
+        };
+      })
+  } else {
+      console.log("error =>", err);
+      return res.redirect('/cuisiniers/login');
+    }
+  })
+};
 
 //export du module
 module.exports = cuisinierController;
