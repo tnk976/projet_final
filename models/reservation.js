1918e9f5acdc6c9fc183771cd54f4c17d4674c9f
HOUMADI BADRANE <badrane976@gmailcom>
2018-07-08 08:51:15 +0400

    ajout model reservation et ajout lien dans navbar vers particulier et cuisinier

---
+++ models/reservation.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var reservationSchema = new mongoose.Schema({
+    //recupere les id des collection produit et magasin
+    id_particulier:[{ type: Schema.Types.ObjectId, ref: 'particulier' }],
+    id_cuisinier: [{ type: Schema.Types.ObjectId, ref: 'cuisinier' }]
+});
+
+
+
+module.exports = mongoose.model("Reservation", reservationSchema);
